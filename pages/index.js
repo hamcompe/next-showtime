0ce2bcd967a26f640e8f6e7a563e28bd74b90be2
Ham <pallop.chao@gmail.com>
2018-03-11 15:13:24 +0700

    :tada: initial project

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Title = styled.div`
+  text-align: center;
+`
+
+function Page() {
+  return (
+    <Title>
+      <h1>Hello Setup Next.js</h1>
+    </Title>
+  )
+}
+
+export default Page
