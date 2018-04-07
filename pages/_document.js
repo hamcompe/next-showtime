9faaff0bf7c6df086baad03f05bc2d9244d23112
Ham <pallop.chao@gmail.com>
2018-04-07 17:06:11 +0700

    🎁 Try Add PWA header tags and icon

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,14 @@ export default class MyDocument extends Document {
         <Head>
           <title>Next Showtime</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+          <meta name="theme-color" content="#0068ef" />
+          <link rel="apple-touch-icon" href="/static/owl.png" />
+          <meta name="apple-mobile-web-app-title" content="Next Showtime" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
+
           {this.props.styleTags}
         </Head>
         <body>
